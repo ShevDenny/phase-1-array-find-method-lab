e5410b6f6a4fbf2e91d452a15645c2c6c3a203e1
ShevDenny <sdenny904@gmail.com>
2021-05-23 17:51:34 -0400

    commit tree 1c33c9ced58404f1c7b3ea85881982968816964a

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
+const record = [
+    {year: '2018', result:'L'},
+    {year: '2017', result:'W'},
+    {year: '2016', result: 'N/A'}
+]
 
+// function superbowlWin(record) {
+//     let result = record.find(record => record.result === 'W');
+//     if (result = 'W') {
+//         return result.year
+//     } else {
+//         return undefined
+//     }
+    
+// }
+
+// Why does this work??
+
+superbowlWin = (record) => {
+    const result = record.find( record => record.result === "W" );
+    return !!result ? result.year : undefined;
+  }
